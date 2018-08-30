b4e565aba0404e1a60e8ea7d859d18b9588b54a9
Lee Li <lichunhui1992@gmail.com>
2018-08-29 21:59:08 -0500

    connect 4 main js file updated

--- Django/conc4.js
+++ Django/conc4.js
@@ -3,14 +3,28 @@ console.log('Connected')
 // yellow rgb(255, 255, 0)
 // gray rgb(128, 128, 128)
 var gameon = true;
-var p1name = prompt('Please enter the name of Player one')
+
+
+while (true) {
+  var p1name = prompt('Please enter the name of Player one');
+  if (p1name == '' || p1name == null) {
+    alert('Please enter something');
+  }else {
+    break;
+  }
+}
+
+
+
 var turn = 1;
 while (true) {
-  var p2name = prompt('Please enter the name of Player Two')
+  var p2name = prompt('Please enter the name of Player Two');
   if (p2name === p1name) {
-    alert('Please enter a different name')
+    alert('Please enter a different name');
+  }else if (p2name ===''|| p2name == null) {
+    alert('Please enter something');
   }else {
-    break
+    break;
   }
 }
 
@@ -29,17 +43,17 @@ function horiCheck (){
       if (TG(i,j).css('background-color') !== 'rgb(128, 128, 128)' && TG(i,j).css('background-color') == TG(i,j+1).css('background-color') && TG(i,j).css('background-color') == TG(i,j+2).css('background-color') && TG(i,j).css('background-color') == TG(i,j+3).css('background-color')) {
         return true;
       }
-      }
+    }
   }
 }
 
 function vertCheck (){
-  for (var i = 0; i < 7; i++) {
-    for (var j = 5; j > 2; j--) {
-      if (TG(i,j).css('background-color') !== 'rgb(128, 128, 128)' && TG(j,i).css('background-color') == TG(j-1,i).css('background-color') && TG(j,i).css('background-color') == TG(j-2,i).css('background-color') && TG(j,i).css('background-color') == TG(j-3,i).css('background-color')) {
+  for (var j = 0; j < 7; j++)   {
+    for (var i = 5; i > 2; i--) {
+      if (TG(i,j).css('background-color') !== 'rgb(128, 128, 128)' && TG(i,j).css('background-color') == TG(i-1,j).css('background-color') && TG(i,j).css('background-color') == TG(i-2,j).css('background-color') && TG(i,j).css('background-color') == TG(i-3,j).css('background-color')) {
         return true;
       }
-      }
+    }
   }
 }
 
@@ -49,6 +63,7 @@ function diaCheck (){
       if (TG(i,j).css('background-color') !== 'rgb(128, 128, 128)' && TG(i,j).css('background-color') == TG(i-1,j+1).css('background-color') && TG(i,j).css('background-color') == TG(i-2,j+2).css('background-color') && TG(i,j).css('background-color') == TG(i-3,j+3).css('background-color')) {
         return true;
       }
+
       }
   }
 }
@@ -63,9 +78,13 @@ function diaCheck2 (){
 }
 function wincondit (){
   one = horiCheck();
+  console.log(one);
   two = vertCheck();
+  console.log(two);
   three = diaCheck();
+  console.log(three);
   four = diaCheck2();
+  console.log(three);
   if (one||two||three||four) {
     return true;
   }
@@ -85,32 +104,29 @@ function BottomAva(c){
 var current = p1name
 $('h4').text(p1name + ': this is your turn to play.')
 var color = 'rgb(255, 0, 0)'
-
-table.find('button').on('click',function () {
-    var c = $(this).closest("td").index();
-    var r = $(this).closest("tr").index();
-    console.log('the row is ',r)
-    console.log('the column is ',c)
-    r = BottomAva(c)
-    console.log('avaliable r ',r)
-    if (r!=undefined) {
-      changeColor (r,c,color);
-      turn = turn + 1;
-      if (turn%2 === 0) {
-        current = p2name
-        $('h4').text(current + ': this is your turn to play.')
-        color = 'rgb(255, 255, 0)'
-      }else {
-        current = p1name
-        $('h4').text(current + ': this is your turn to play.')
-        color = 'rgb(255, 0, 0)'
+  table.find('button').on('click',function () {
+      var c = $(this).closest("td").index();
+      var r = $(this).closest("tr").index();
+      r = BottomAva(c)
+      if (r!=undefined) {
+        changeColor (r,c,color);
+        turn = turn + 1;
+        a = wincondit()
+        if (a) {
+          $('h4').text('Player:' + current + ' has won!')
+          gameon = false;
+        }else {
+          if (turn%2 === 0) {
+            current = p2name
+            $('h4').text(current + ': this is your turn to play.')
+            color = 'rgb(255, 255, 0)'
+          }else {
+            current = p1name
+            $('h4').text(current + ': this is your turn to play.')
+            color = 'rgb(255, 0, 0)'
+          }
+        }
+      }else{
+        alert('This column is full\n Please select another column')
       }
-      a = wincondit()
-      if (a) {
-        alert('Player:' + current + ' has won!')
-      }
-
-    }else{
-      alert('This column is full\n Please select another column')
-    }
-})
+  })
