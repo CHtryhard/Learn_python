54e05fd496996057c931918e1c1c459345fad160
Lee Li <lichunhui1992@gmail.com>
2018-09-26 20:24:21 -0500

    Class based view update, Template based view to be continued

---
+++ advanced_section/advcbv/basic_app/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render
+from django.views.generic import View, TemplateView
+from django.http import HttpResponse
+
+
+
+# Create your views here.
+# def index(request):
+#     return render(request, 'index.html')
+
+# class CBView(View):
+#     def get(self, request):
+#         return HttpResponse("class based view")
+
+class IndexView(TemplateView):
+    template_name = 'index.html'
+
+    def get_context_data(self,**kwargs):
+        context = super().get_context_data(**kwargs)
+        context['inject1'] = '123456'
+        return context
