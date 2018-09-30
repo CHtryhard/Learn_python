adbdca4afdf2ac610696817c6ea88165b120253b
Lee Li <lichunhui1992@gmail.com>
2018-09-29 23:16:29 -0500

    update 1 finished registration part 1

--- advanced_section/advcbv/basic_app/views.py
+++ advanced_section/advcbv/basic_app/views.py
@@ -1,21 +1,19 @@
 from django.shortcuts import render
-from django.views.generic import View, TemplateView
-from django.http import HttpResponse
-
-
-
-# Create your views here.
-# def index(request):
-#     return render(request, 'index.html')
-
-# class CBView(View):
-#     def get(self, request):
-#         return HttpResponse("class based view")
+from django.views.generic import View, TemplateView, ListView, DetailView
+from . import models
 
 class IndexView(TemplateView):
     template_name = 'index.html'
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs)
-        context['inject1'] = '123456'
-        return context
+
+class SchoolListView(ListView):
+    context_object_name = 'schools'
+    model=models.School
+    # without the context it return lower case of School + _list: school_list
+
+class SchoolDetailView(DetailView):
+    context_object_name = 'school_detail'
+    # without the context it return lower case of School
+    model = models.School
+
+    template_name = 'basic_app/school_detail.html'
