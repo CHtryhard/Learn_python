d072ca41a71dfa3c2748bb54631a2a0f1e50e99e
CHtryhard <38825242+CHtryhard@users.noreply.github.com>
2018-10-02 16:31:45 -0500

    clone project first update

---
+++ portfolio_web/mysite/blog/forms.py
@@ -0,0 +1,24 @@
+from django import forms
+from blog.models import Post, Comment
+
+class PostForm(forms.ModelForm):
+
+    class Meta():
+        model = Post
+        fiedls = ('author','title','text')
+
+        widgets = {
+        'title':forms.TextInput(attrs={'class':'textinputclass'}),
+        'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
+        }
+
+class CommentForm(forms.ModelForm):
+
+    class Meta():
+        model = Comment
+        fiedls = ('author','text')
+
+        widgets={
+
+
+        }
